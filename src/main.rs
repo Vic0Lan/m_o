6908d84ab1566cdc32e71966936db73341eb0740
Vic Lan <91805528+Vic0Lan@users.noreply.github.com>
2024-05-05 17:14:43 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::env;
 use std::fs;
 use std::io::{Error, ErrorKind};
 use std::path::{Path, PathBuf};
-use std::thread;
 
 // Crea una funzione per eseguire il clean della directory ✅
 // Crea una funzione per attraversare le directory da pulire ✅
@@ -12,13 +11,16 @@ use std::thread;
 // Implementa output migliorato (usa owo_colors ad esempio) ✅
 // Implementa output verboso ✅
 //
-// Progetto finito per il momento!!!
+// Implementa la ricorsione delle cartelle ✅
+// Sistema l'error handling di clean in relazione ai file
 
-static mut VERBOSE: bool = false;
-
-fn clean(path: &Path) -> Result<(), Error> {
+pub fn clean(path: &Path) -> Result<(), Error> {
     //[ Controllo che la cartella esiste ]
     let flag = if path.exists() { true } else { false };
+
+    if path.is_file() {
+        return Ok(());
+    }
 
     let path_name = path
         .to_str()
@@ -31,19 +33,17 @@ fn clean(path: &Path) -> Result<(), Error> {
     //[ Rimuovo la cartella ]
     fs::remove_dir_all(path).unwrap_or_else(|err| match Error::kind(&err) {
         ErrorKind::NotFound => {
-            if unsafe { VERBOSE == true } {
-                println!(
-                    "Target folder not found: {:?}",
-                    folder_name
-                        .expect(
-                            "failed to obtain folder name"
-                                .on_bright_red()
-                                .to_string()
-                                .as_str()
-                        )
-                        .yellow()
-                );
-            }
+            println!(
+                "Target folder not found: {:?}",
+                folder_name
+                    .expect(
+                        "failed to obtain folder name"
+                            .on_bright_red()
+                            .to_string()
+                            .as_str()
+                    )
+                    .yellow()
+            );
         }
         _ => eprintln!("Another kind of error: {}", Error::kind(&err).on_red()),
     });
@@ -64,54 +64,56 @@ fn clean(path: &Path) -> Result<(), Error> {
     Ok(())
 }
 
-fn traverse_directories(dir: &str) -> Result<(), Error> {
-    let dirs = fs::read_dir(dir)?;
-    let mut handlers = Vec::new();
+fn traverse_directories(path: &PathBuf) -> Result<(), Error> {
+    for entry in fs::read_dir(path)? {
+        let entry = match entry.as_ref() {
+            Ok(entry) => entry,
+            Err(_) => continue,
+        };
 
-    for entry in dirs {
-        let handler = thread::spawn(|| -> Result<(), Error> {
-            let mut buffer = PathBuf::new();
+        if entry.path().ends_with(".git") {
+            return Ok(());
+        }
 
-            buffer.push(entry?.path());
-            buffer.push("target");
+        if entry.path().ends_with("target") || entry.path().ends_with("release") {
+            clean(entry.path().as_path())?;
+        }
 
-            clean(&buffer)?;
-            Ok(())
-        });
-
-        handlers.push(handler);
-    }
-
-    for handler in handlers {
-        handler.join().expect(
-            "failed to await process"
-                .on_bright_red()
-                .to_string()
-                .as_str(),
-        )?;
-    }
-
-    Ok(())
-}
-
-fn main() {
-    println!("Hi i am Mo and im a here to clean ");
-    println!("Cleaning...");
-
-    let args = env::args().collect::<Vec<_>>();
-
-    if let Some(str) = args.get(2) {
-        if str == "--verbose" {
-            unsafe {
-                VERBOSE = true;
-            }
+        if entry.path().is_dir() {
+            traverse_directories(&entry.path())?;
         }
     }
 
-    let out = traverse_directories(
+    return Ok(());
+}
+
+fn help() {
+    println!(
+        "{}",
+        "Hello, to use Mo type: \nm_o <path_to_folder>".green()
+    );
+
+    println!(
+        "{}",
+        "You can also add the --verbose flag or verbose output: \nm_o <path_to_folder> --verbose"
+            .green()
+    );
+}
+
+fn main() {
+    let args = env::args().collect::<Vec<_>>();
+
+    if args.len() < 2 {
+        help();
+        return;
+    }
+
+    println!("Mo ready for cleaning");
+
+    let out = traverse_directories(&PathBuf::from(
         args.get(1)
             .expect("failed to read argument".on_red().to_string().as_str()),
-    );
+    ));
 
     match out {
         Ok(_) => println!("{}", "Your space is now freed, bye bye".green()),
